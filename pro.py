a1132f27cb31821a7d906fb76c43794c352f4c9d
dayana.astana <dayana.astana@gmail.com>
2018-12-13 22:29:08 +0600

    Add exceptions and PixMap

--- pro.py
+++ pro.py
@@ -1,5 +1,6 @@
 import sys
 
+from PIL.ImageQt import ImageQt
 from PyQt5.Qt import Qt
 
 from PyQt5 import uic
@@ -21,21 +22,52 @@ class MyWidget(QMainWindow):
         super().__init__()
         self.label = QLabel(self)
         uic.loadUi('untitled.ui', self)
-
+        self.fname = ''
+        self.im = ''
         self.obzor.clicked.connect(self.obzor_f)
+        self.save.clicked.connect(self.save_f)
         self.filtrs.clicked.connect(self.filtrs_f)
         self.contrast.clicked.connect(self.contrast_f)
         self.rotate.clicked.connect(self.rotate_f)
         self.brightness.clicked.connect(self.brightness_f)
         self.text.clicked.connect(self.text_f)
+        self.save.setEnabled(False)
+        self.contrast.setEnabled(False)
+        self.rotate.setEnabled(False)
+        self.brightness.setEnabled(False)
+        self.text.setEnabled(False)
+        self.filtrs.setEnabled(False)
+
+
+
+
+
+
+    def save_f(self):
+        self.imName, _ = QFileDialog.getSaveFileName(self, "Save File", "",  "IMAGE FILES (*.JPEG)")
+        self.im.save(self.imName, 'jpeg')
+
+
+
 
     def obzor_f(self):
-        fname = QFileDialog.getOpenFileName(self, 'Open file', '/home')[0]
-        global f
-        f = open(fname, 'r')
+        self.fname = QFileDialog.getOpenFileName(self, 'Open file', '/home')[0]
+        self.pixmap_f(self.fname)
+        if self.fname != '':
+            self.save.setEnabled(True)
+            self.contrast.setEnabled(True)
+            self.rotate.setEnabled(True)
+            self.brightness.setEnabled(True)
+            self.text.setEnabled(True)
+            self.filtrs.setEnabled(True)
 
-        pixmap = QPixmap(f.name)
 
+    def pixmap_f(self, im):
+        if isinstance(im, str):
+
+            pixmap = QPixmap(im)
+        else:
+            pixmap = QPixmap.fromImage(im)
         self.label.setPixmap(pixmap)
         myScaledPixmap = pixmap.scaled(self.label.size(), Qt.KeepAspectRatio)
         self.label.setPixmap(myScaledPixmap)
@@ -53,55 +85,78 @@ class MyWidget(QMainWindow):
         )
         if okBtnPressed:
             filtr = i
-            self.filtr_edit(filtr, f.name)
+
+            self.filtr_edit(filtr, self.fname)
 
     def filtr_edit(self, filtr, file):
-            im = Image.open(file)
-            pixels = im.load()  # список с пикселями
-            x, y = im.size
+            self.im = Image.open(file)
+            pixels = self.im.load()  # список с пикселями
+            x, y = self.im.size
             if filtr == 'DISCOLORED':
                 for i in range(x):
                     for j in range(y):
                         r, g, b = pixels[i, j]
                         bw = (r + g + b) // 3
                         pixels[i, j] = bw, bw, bw
-                im.save('/home/odmin/DISCOLORED.jpg')
+                self.pixmap_f(ImageQt(self.im))
+
+                # im.save('/home/odmin/DISCOLORED.jpg')
             if filtr == 'INVERSION':
                 for i in range(x):
                     for j in range(y):
                         r, g, b = pixels[i, j]
                         pixels[i, j] = 255 - r, 255 - g, 255 - b
-                im.save('/home/odmin/INVERSION.jpg')
+                self.pixmap_f(ImageQt(self.im))
+
+                # im.save('/home/odmin/INVERSION.jpg')
             if filtr == 'BLUR':
-                out_im = im.filter(ImageFilter.BLUR)
-                out_im.save('/home/odmin/BLUR.jpg')
+                self.im = self.im.filter(ImageFilter.BLUR)
+                self.pixmap_f(ImageQt(self.im))
+
+                # out_im.save('/home/odmin/BLUR.jpg')
             if filtr == 'CONTOUR':
-                out_im = im.filter(ImageFilter.CONTOUR)
-                out_im.save('/home/odmin/CONTOUR.jpg')
+                self.im = self.im.filter(ImageFilter.CONTOUR)
+                self.pixmap_f(ImageQt(self.im))
+
+                # out_im.save('/home/odmin/CONTOUR.jpg')
             if filtr == 'DETAIL':
-                out_im = im.filter(ImageFilter.DETAIL)
-                out_im.save('/home/odmin/DETAIL.jpg')
+                self.im = self.im.filter(ImageFilter.DETAIL)
+                self.pixmap_f(ImageQt(self.im))
+
+                # out_im.save('/home/odmin/DETAIL.jpg')
             if filtr == 'EDGE_ENHANCE':
-                out_im = im.filter(ImageFilter.EDGE_ENHANCE)
-                out_im.save('/home/odmin/EDGE_ENHANCE.jpg')
+                self.im = self.im.filter(ImageFilter.EDGE_ENHANCE)
+                self.pixmap_f(ImageQt(self.im))
+
+                # out_im.save('/home/odmin/EDGE_ENHANCE.jpg')
             if filtr == 'EDGE_ENHANCE_MORE':
-                out_im = im.filter(ImageFilter.EDGE_ENHANCE_MORE)
-                out_im.save('/home/odmin/EDGE_ENHANCE_MORE.jpg')
+                self.im = self.im.filter(ImageFilter.EDGE_ENHANCE_MORE)
+                self.pixmap_f(ImageQt(self.im))
+
+                # out_im.save('/home/odmin/EDGE_ENHANCE_MORE.jpg')
             if filtr == 'EMBOSS':
-                out_im = im.filter(ImageFilter.EMBOSS)
-                out_im.save('/home/odmin/EMBOSS.jpg')
+                self.im = self.im.filter(ImageFilter.EMBOSS)
+                self.pixmap_f(ImageQt(self.im))
+                # out_im.save('/home/odmin/EMBOSS.jpg')
             if filtr == 'FIND_EDGES':
-                out_im = im.filter(ImageFilter.FIND_EDGES)
-                out_im.save('/home/odmin/FIND_EDGES.jpg')
+                self.im = self.im.filter(ImageFilter.FIND_EDGES)
+                self.pixmap_f(ImageQt(self.im))
+                # out_im.save('/home/odmin/FIND_EDGES.jpg')
             if filtr == 'SMOOTH':
-                out_im = im.filter(ImageFilter.SMOOTH)
-                out_im.save('/home/odmin/SMOOTH.jpg')
+                self.im = self.im.filter(ImageFilter.SMOOTH)
+                self.pixmap_f(ImageQt(self.im))
+
+                # out_im.save('/home/odmin/SMOOTH.jpg')
             if filtr == 'SHARPEN':
-                out_im = im.filter(ImageFilter.SHARPEN)
-                out_im.save('/home/odmin/SHARPEN.jpg')
+                self.im = self.im.filter(ImageFilter.SHARPEN)
+                self.pixmap_f(ImageQt(self.im))
+
+                # out_im.save('/home/odmin/SHARPEN.jpg')
             if filtr == 'SMOOTH_MORE':
-                out_im = im.filter(ImageFilter.SMOOTH_MORE)
-                out_im.save('/home/odmin/SMOOTH_MORE.jpg')
+                self.im = self.im.filter(ImageFilter.SMOOTH_MORE)
+                self.pixmap_f(ImageQt(self.im))
+
+                # out_im.save('/home/odmin/SMOOTH_MORE.jpg')
 
     def rotate_f(self):
         i, okBtnPressed = QInputDialog.getText(
@@ -109,18 +164,20 @@ class MyWidget(QMainWindow):
         )
         if okBtnPressed:
             value_gradus = i
-            self.rotate_edit(value_gradus, f.name)
+            self.rotate_edit(value_gradus, self.fname)
 
     def rotate_edit(self, value_gradus, file):
-        im = Image.open(file)  # открываем картинку
-        out_im = im.rotate(int(value_gradus))
-        out_im.save('/home/odmin/ROTATE.jpg')
+        self.im = Image.open(file)  # открываем картинку
+        self.im = self.im.rotate(int(value_gradus))
+        self.pixmap_f(ImageQt(self.im))
+        # im.save('/home/odmin/ROTATE.jpg')
 
-    def text_edit(self, value_x, value_y, text_im, text_color, font):
-        im = Image.open(f.name)
-        draw = ImageDraw.Draw(im)
+    def text_edit(self, value_x, value_y, text_im, text_color, font, file):
+        self.im = Image.open(file)
+        draw = ImageDraw.Draw(self.im)
         draw.text((int(value_x), int(value_y)), text_im, text_color, font=font)
-        im.save("/home/odmin/TEXT.jpg")
+        self.pixmap_f(ImageQt(self.im))
+        # im.save("/home/odmin/TEXT.jpg")
 
     def text_f(self):
         global text_im, text_color, font
@@ -164,9 +221,9 @@ class MyWidget(QMainWindow):
         elif font == 'Symbola_hint':
             font = ImageFont.truetype('Symbola_hint.ttf', int(value_font))
         elif font == 'Ubuntu_B':
-            font = ImageFont.truetype('Ubuntu_B.ttf', int(value_font))
+            font = ImageFont.truetype('Ubuntu-B.ttf', int(value_font))
 
-        self.text_edit(value_x, value_y, text_im, text_color, font)
+        self.text_edit(value_x, value_y, text_im, text_color, font, self.fname)
 
     def brightness_f(self):
         i, okBtnPressed = QInputDialog.getText(
@@ -178,8 +235,8 @@ class MyWidget(QMainWindow):
 
 
     def brightness_edit(self, brightness):
-        im = f.name
-        source = Image.open(im)
+        self.im = self.fname
+        source = Image.open(self.im)
         result = Image.new('RGB', source.size)
         for x in range(source.size[0]):
             for y in range(source.size[1]):
@@ -195,7 +252,8 @@ class MyWidget(QMainWindow):
                 blue = min(255, max(0, blue))
 
                 result.putpixel((x, y), (red, green, blue))
-        result.save('/home/odmin/BRIGHTNESS.jpg')
+        self.pixmap_f(ImageQt(result))
+        # result.save('/home/odmin/BRIGHTNESS.jpg')
 
     def contrast_f(self):
         i, okBtnPressed = QInputDialog.getText(
@@ -206,8 +264,8 @@ class MyWidget(QMainWindow):
             self.contrast_edit(contrast)
 
     def contrast_edit(self, contrast):
-        source = Image.open(f.name)
-        result = Image.new('RGB', source.size)
+        source = Image.open(self.fname)
+        self.im = Image.new('RGB', source.size)
 
         avg = 0
         for x in range(source.size[0]):
@@ -228,11 +286,9 @@ class MyWidget(QMainWindow):
         for x in range(source.size[0]):
             for y in range(source.size[1]):
                 r, g, b = source.getpixel((x, y))
-                result.putpixel((x, y), (palette[r], palette[g], palette[b]))
-
-        result.save("/home/odmin/CONTRAST.jpg")
-
-
+                self.im.putpixel((x, y), (palette[r], palette[g], palette[b]))
+        self.pixmap_f(ImageQt(self.im))
+        # result.save("/home/odmin/CONTRAST.jpg")
 
 
 app = QApplication(sys.argv)
